e8fa69bff7a2b2fe1c09e7e4af360dd55c50cb58
vindhyavikas54 <159005819+vindhyavikas54@users.noreply.github.com>
2025-03-19 22:19:46 +0530

    Updatewebapp.py

--- webapp.py
+++ webapp.py
@@ -39,7 +39,7 @@ if 'page' not in st.session_state:
 # Define the main page
 def main_page():
     st.markdown('<div class="center">', unsafe_allow_html=True)
-    st.image(r"assets\Sign Language UI.png", use_container_width=True)
+    st.image(r"assets/Sign Language UI.png", use_container_width=True)
     st.title("American Sign Language")
     if st.button("Start", key="first_image_button"):
         st.session_state['page'] = 'first_image'
@@ -48,14 +48,14 @@ def main_page():
 # Define the first image page
 def first_image_page():
     st.title("First Image")
-    st.image(r"assets\Signs-Part1.jpg", use_container_width=True)
+    st.image(r"assets/Signs-Part1.jpg", use_container_width=True)
     if st.button("Next", key="second_image_button"):
         st.session_state['page'] = 'second_image'
 
 # Define the second image page
 def second_image_page():
     st.title("Second Image")
-    st.image(r"assets\Signs -Part2.jpg", use_container_width=True)
+    st.image(r"assets/Signs -Part2.jpg", use_container_width=True)
     if st.button("Back to Home", key="back_home_button"):
         st.session_state['page'] = 'main_page'
 
